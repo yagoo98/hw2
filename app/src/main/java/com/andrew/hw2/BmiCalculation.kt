290c7c3ee8807c9d139ed87756477939df5499ea
andrew <AndrewChu@soetek.com.tw>
2024-08-14 16:18:34 +0800

    v1.0

---
+++ app/src/main/java/com/andrew/hw2/BmiCalculation.kt
@@ -0,0 +1,43 @@
+package com.andrew.hw2
+
+import kotlin.math.round
+
+class BmiCalculation {
+    enum class WeightRank {
+        UNDERWEIGHT, HEALTHY, OVERWEIGHT, OBESE, EXOBESE, WARNING
+    }
+
+    enum class Message {
+        WEIGHT, HEIGHT, ALL, NORMAL
+    }
+
+    fun getBmi(height: Double, weight: Double): Double {
+        return round(weight / (height * height) * 100) / 100
+    }
+
+    private fun getWeightRank(bmi: Double): WeightRank {
+        return when {
+            0.0 <= bmi && bmi < 18.5 -> WeightRank.UNDERWEIGHT
+            18.5 <= bmi && bmi < 24.0 -> WeightRank.HEALTHY
+            24.0 <= bmi && bmi < 30.0 -> WeightRank.OVERWEIGHT
+            30.0 <= bmi && bmi < 35.0 -> WeightRank.OBESE
+            35.0 <= bmi -> WeightRank.EXOBESE
+            else -> WeightRank.WARNING
+        }
+    }
+
+    fun getMessage(height: Double, weight: Double): Message {
+        return when {
+            height > 0 && weight == 0.0 -> Message.WEIGHT
+            height == 0.0 && weight > 0 -> Message.HEIGHT
+            height == 0.0 && weight == 0.0 -> Message.ALL
+            else -> Message.NORMAL
+        }
+    }
+
+    fun getTitle(height: Double, weight: Double): WeightRank {
+        val bmi = getBmi(height, weight)
+
+        return getWeightRank(bmi)
+    }
+}
--- app/src/main/java/com/andrew/hw2/MainActivity.kt
+++ app/src/main/java/com/andrew/hw2/MainActivity.kt
@@ -1,47 +1,54 @@
 package com.andrew.hw2
 
+import android.app.AlertDialog
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import android.view.View
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.andrew.hw2.ui.theme.Hw2Theme
+import androidx.appcompat.app.AppCompatActivity
+import com.andrew.hw2.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            Hw2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+        setContentView(binding.root)
+    }
+
+    fun calculate(view: View) {
+        val height = binding.heightInput.text.toString().ifBlank { "0.0" }.toDouble()
+        val weight = binding.weightInput.text.toString().ifBlank { "0.0" }.toDouble()
+
+        val bmiCalculation = BmiCalculation()
+        val title = when (bmiCalculation.getTitle(height, weight)) {
+            BmiCalculation.WeightRank.UNDERWEIGHT -> getString(R.string.underweight)
+            BmiCalculation.WeightRank.HEALTHY -> getString(R.string.healthy)
+            BmiCalculation.WeightRank.OVERWEIGHT -> getString(R.string.overweight)
+            BmiCalculation.WeightRank.OBESE -> getString(R.string.obese)
+            BmiCalculation.WeightRank.EXOBESE -> getString(R.string.extremely_obese)
+            BmiCalculation.WeightRank.WARNING -> getString(R.string.error_title)
+        }
+        val message = when (bmiCalculation.getMessage(height, weight)) {
+            BmiCalculation.Message.WEIGHT -> getString(R.string.please_enter_your_weight)
+            BmiCalculation.Message.HEIGHT -> getString(R.string.please_enter_your_height)
+            BmiCalculation.Message.ALL -> getString(R.string.please_enter_your_height_and_weight)
+            BmiCalculation.Message.NORMAL -> "bmi= ${bmiCalculation.getBmi(height, weight)} ${
+                getString(R.string.bmi_unit)
+            }"
+        }
+
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton(getString(R.string.ok)) { _, _ ->
+                if (height * weight > 0) {
+                    binding.heightInput.setText(getString(R.string.empty))
+                    binding.weightInput.setText(getString(R.string.empty))
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Hw2Theme {
-        Greeting("Android")
+            .setNegativeButton("cancel", null)
+            .show()
     }
 }
